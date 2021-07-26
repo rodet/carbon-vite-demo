55254721b533a8406b6e7d189eddfd4cf5434c2c
Stephane Rodet <s@rodet.org>
2021-07-26 10:35:31 +0200

    feat: add Carbon and SCSS
    Also feature a header component example to load in a bit more Carbon CSS and functionality.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import './App.scss'
+import { Button } from 'carbon-components-react';
+import TutorialHeader from './components/header/index.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <TutorialHeader></TutorialHeader>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+          <Button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
-          </button>
+          </Button>
         </p>
         <p>
           Edit <code>App.jsx</code> and save to test HMR updates.
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import './styles/carbon.scss'
+import './styles/general.scss'
 import App from './App'
 
 ReactDOM.render(
